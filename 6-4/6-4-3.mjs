32b7a9cb7665548d5b653e8f43a91e108b463fa8
popotan <nkhshc421023@g.neec.ac.jp>
2024-12-23 16:48:56 +0900

    6-4-3

---
+++ 6-4/6-4-3.mjs
@@ -0,0 +1,11 @@
+const value = 70
+
+if (value>=90) {
+    console.log("Excellent")
+} else if (value>=80) {
+    console.log("Good")
+} else if (value>=60) {
+    console.log("Normal")
+} else {
+    console.log("Red")
+} 
